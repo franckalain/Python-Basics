e903ab66780edee93a4b7f4e3705cebe7b81b933
Franck Alain Kouadio <49621147+franckalain@users.noreply.github.com>
2019-09-18 16:38:20 +0000

    Add files via upload

---
+++ Variables/first_module.py
@@ -0,0 +1,10 @@
+print("This will always be run")
+
+def main():
+    print("First Module's Main Method")
+if __name__ == "__main__":
+    main()
+#if __name__ == "__main__":
+    #print("run directly")
+#else:
+    #print('run from Import')
